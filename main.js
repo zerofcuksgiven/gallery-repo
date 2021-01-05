0d5266208d088c1ebeb9aae9629d8dca0c29bcb2
Gian Carlo <glincuna@gmail.com>
2021-01-05 18:20:32 -0500

    pushing first update!
    need to finish darken/lighten button

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
+for(let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+    displayedImage.src = e.target.src;
+  }
+}
 /* Wiring up the Darken/Lighten button */
+btn.getAttribute('class', xxx);
+btn.textContent = xxx;
+overlay.style.backgroundColor = xxx;
